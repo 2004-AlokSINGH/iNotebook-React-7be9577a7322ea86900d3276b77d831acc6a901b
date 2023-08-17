cfe7a0acd2915ac91b47686ab7f648d6de80741a
ALOK SINGH <thakuraloksingh186@gmail.com>
2023-08-17 22:40:44 +0530

    Adding fond Awesome

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React,{useContext,useEffect} from 'react'
+import noteContext from '../context/Notes/noteContext'
+
+function About() {
+ 
+  return (
+    <div>
+        This is about page
+    </div>
+  )
+}
+
+export default About
